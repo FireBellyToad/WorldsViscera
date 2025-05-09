c4775137009ab1d33c61d88565ea57788897e381
Faust <jacopo.buttiglieri@gmail.com>
2025-05-09 17:47:13 +0200

    left moving enemies

--- Notes.md
+++ Notes.md
@@ -6,4 +6,4 @@ Design pattern different from OOP
 
 * **Entity:** A thing which can have many *Components*
 * **Component:** Data grouped by meaning (examples: Position, Renderable, Hostile...)
-* **System:** Mechanism that gathers and modifies data from Entities and Components 
+* **System:** Mechanism that *gathers* *and* *modifies* data from *Entities* and *Components* 
--- worldsviscera/src/main.rs
+++ worldsviscera/src/main.rs
@@ -13,13 +13,26 @@ struct State {
     ecs_world: World, // World of ECS, where the framework lives
 }
 
+//State common implementations
+impl State {
+
+    // Function taht will create and run a LeftWalker System 
+    fn run_systems(&mut self) {
+        let mut left_walker = LeftWalker{};
+        left_walker.run_now(&self.ecs_world); //Run system, run!
+        self.ecs_world.maintain(); // if any changes are queued by the systems, apply them now to the world
+    }
+}
+
 //GameState Trait implementation needed to create a tick function
 impl GameState for State {
-
     //Code executed on each tick of the Game state
     fn tick(&mut self, context: &mut BTerm) {
         //ctx is a reference to the terminal
         context.cls(); //clean terminal
+
+        // The current state will run!
+        self.run_systems();
 
         //We read Position and Renderable currently inserted in world
         let positions = self.ecs_world.read_storage::<Position>();
@@ -55,6 +68,38 @@ struct Renderable {
     background: RGB,
 }
 
+#[derive(Component)]
+struct LeftMover {}
+
+//Empty structure to attach logic
+struct LeftWalker {}
+
+// Implementing "System" Trait for LeftWalker.
+// The System is asking us what it needs to be done
+// 'a specifies that the lifetime must be long enough to make the System run
+impl<'a> System<'a> for LeftWalker {
+
+    // SystemData is an alias of the tuple (ReadStorage, WriteStorage)
+    // Here we define what kind of SystemData the "run" function will use and how
+    // We can READ LeftMover Components and READ AND WRITE Position components
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    // System Trait implementation
+    // We get the data we defined as SystemData (readalbe LeftMover components and writeable Position components)
+    // and do stuff with it
+    fn run(&mut self, (left_mover, mut position): Self::SystemData) {
+
+        //For each entity that have BOTH LeftMover and Position Component, move left
+        //_left_mover is never used here
+        for (_left_mover, position) in (&left_mover, &mut position).join() {
+            position.x -= 1;
+            if position.x < 0 {
+                position.x = 79;
+            }
+        }
+    }
+}
+
 fn main() -> BError {
     //This is a context. what is this?
     let context = BTermBuilder::simple80x50() //Create a 80x50 basic terminal
@@ -71,6 +116,7 @@ fn main() -> BError {
     //This seems like is working with a Generic / Pseudoreflection mechanism!
     gs.ecs_world.register::<Position>();
     gs.ecs_world.register::<Renderable>();
+    gs.ecs_world.register::<LeftMover>();
 
     //Insert player "@" into world
     gs.ecs_world
@@ -93,6 +139,7 @@ fn main() -> BError {
                 foreground: RGB::named(RED1),
                 background: RGB::named(BLACK),
             })
+            .with(LeftMover {}) // This entity has component LeftMover
             .build();
     }
 
