ae92dc522c53a38052cdcc750034f103d177055d
Faust90 <jacopo.buttiglieri@gmail.com>
2026-02-02 21:04:56 +0100

    Update mushroom_field_builder.rs

--- src/maps/mushroom_field_builder.rs
+++ src/maps/mushroom_field_builder.rs
@@ -24,8 +24,8 @@ impl ZoneFeatureBuilder for MushroomFieldBuilder {
         let mut tiles: Vec<usize> = Vec::new();
         //2 Create a potential fertilized space from 4x4 to 7x7
         let mut size = Roll::dice(1, SIZE_DICE) + SIZE_MODIFIER;
-        let mut x = Roll::dice(1, MAP_WIDTH - size - 1);
-        let mut y = Roll::dice(1, MAP_HEIGHT - size - 1);
+        let mut x = Roll::dice(1, MAP_WIDTH - size) - 1;
+        let mut y = Roll::dice(1, MAP_HEIGHT - size) - 1;
         let mut field_rect = Rect::new_from_i32(x, y, size, size);
 
         for _ in 0..150 {
@@ -53,38 +53,21 @@ impl ZoneFeatureBuilder for MushroomFieldBuilder {
 
             // 2.2 get new size and rect for next iteration
             size = Roll::dice(1, 3) + 3;
-            x = Roll::dice(1, MAP_WIDTH - size - 1);
-            y = Roll::dice(1, MAP_HEIGHT - size - 1);
+            x = Roll::dice(1, MAP_WIDTH - size);
+            y = Roll::dice(1, MAP_HEIGHT - size);
             field_rect = Rect::new_from_i32(x, y, size, size);
         }
 
-        //3 Create a border of fences and fill the rest with the actual field
-        let mut has_opening = false;
+        //3 Fill the the field with mushrooms and owner
         let mut counter = 0;
         let mut owner_opt: Option<Entity> = None;
         for &index in &tiles {
             let (x, y) = Zone::get_xy_from_index(index);
 
-            // Check if the tile is on the border, must be a fence
-            if x == field_rect.x as i32
-                || y == field_rect.y as i32
-                || x == field_rect.x as i32 + size - 1
-                || y == field_rect.y as i32 + size - 1
-            {
-                counter += 1;
-                // In corners guarantee that the border has a fence
-                if has_opening
-                    || (((x - field_rect.x as i32) % (size - 1) == 0)
-                        && ((y - field_rect.y as i32) % (size - 1) == 0))
-                {
-                    zone.tiles[Zone::get_index_from_xy(&x, &y)] = TileType::FieldFence;
-                } else if !has_opening && (counter >= (size * 3) || Roll::dice(1, 4) == 1) {
-                    // Guarantee an open space in the fence
-                    has_opening = true;
-                    owner_opt = Some(Spawn::moleman_farmer(ecs_world, x, y));
-                } else {
-                    zone.tiles[Zone::get_index_from_xy(&x, &y)] = TileType::FieldFence;
-                }
+            counter += 1;
+            if (owner_opt.is_none() && (counter >= size || Roll::dice(1, 4) == 1)) {
+                // Guarantee an open space in the fence
+                owner_opt = Some(Spawn::moleman_farmer(ecs_world, x, y));
             } else {
                 let index = Zone::get_index_from_xy(&x, &y);
                 zone.tiles[index] = TileType::MushroomField;
